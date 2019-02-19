c3356f60c175f3fb2a8582a8e24d3707e663b3e3
Ash Blue <charles.brindle@wipro.com>
2019-02-19 22:29:57 +0000

    Merged in develop (pull request #2)
    Merged in feature/cloud-build-testing (pull request #1)
    
    Feature/cloud build testing
    
    * Added CI support to the app
    
    * Bugfix for broken e2e test
    
    Approved-by: Spencer Hilvitz <spencer.hilvitz@wipro.com>

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to dev-skill-entry!');
+    expect(page.getTitleText()).toEqual('Dev Skill Entry');
   });
 
   afterEach(async () => {
